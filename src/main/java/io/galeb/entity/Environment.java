87623e58e330cc1683cbe4ad8040124b023e0c1b
Igor Fernandes <igor.fernandes@corp.globo.com>
2015-08-04 14:53:10 -0300

    Add farm relationship

--- src/main/java/io/galeb/entity/Environment.java
+++ src/main/java/io/galeb/entity/Environment.java
@@ -1,16 +1,21 @@
 package io.galeb.entity;
 
 import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Version;
 
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.util.Assert;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 public class Environment {
@@ -27,6 +32,10 @@ public class Environment {
 
     @Column(unique = true, nullable = false)
     private String name;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "environment")
+    private final Set<Farm> farms = new HashSet<>();
 
     public Environment(String name) {
         Assert.hasText(name);
