7c29f3eebbf849f8f7f01d7d30f9c0114f12c86e
Marcelo Monteiro <m.monteiro@corp.globo.com>
2015-08-10 16:27:51 -0300

    GalebV3 driver improved

--- src/main/java/io/galeb/engine/impl/GalebV3Driver.java
+++ src/main/java/io/galeb/engine/impl/GalebV3Driver.java
@@ -1,7 +1,25 @@
 package io.galeb.engine.impl;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.apache.http.HttpEntityEnclosingRequest;
+import org.apache.http.HttpHeaders;
+import org.apache.http.HttpHost;
+import org.apache.http.HttpResponse;
+import org.apache.http.annotation.NotThreadSafe;
+import org.apache.http.client.methods.HttpEntityEnclosingRequestBase;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.springframework.http.MediaType;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
 
 import io.galeb.engine.Driver;
 import io.galeb.handler.VirtualHostHandler;
@@ -24,9 +42,78 @@ public class GalebV3Driver implements Driver {
         String api = properties.getOrDefault("api", "NULL").toString();
         String json = properties.getOrDefault("json", "{}").toString();
         String path = properties.getOrDefault("path", "").toString();
-        LOGGER.info(">>>>>>>>>>> POST http://" + api + "/" + path);
-        LOGGER.info(json);
-        return true;
+        String uriPath = "http://" + api + "/" + path;
+        RestTemplate restTemplate = new RestTemplate();
+        boolean result = false;
+
+        try {
+            URI uri = new URI(uriPath);
+            RequestEntity<String> request = RequestEntity.post(uri).contentType(MediaType.APPLICATION_JSON).body(json);
+            ResponseEntity<String> response = restTemplate.exchange(request, String.class);
+            result = response.getStatusCode().value() < 400;
+        } catch (RuntimeException|URISyntaxException e) {
+            LOGGER.error("POST "+uriPath+" ("+e.getMessage()+")");
+        }
+        return result;
     }
 
+    @Override
+    public boolean update(Properties properties) {
+        String api = properties.getOrDefault("api", "NULL").toString();
+        String json = properties.getOrDefault("json", "{}").toString();
+        String path = properties.getOrDefault("path", "").toString();
+        String uriPath = "http://" + api + "/" + path;
+        RestTemplate restTemplate = new RestTemplate();
+        boolean result = false;
+
+        try {
+            URI uri = new URI(uriPath);
+            RequestEntity<String> request = RequestEntity.put(uri).contentType(MediaType.APPLICATION_JSON).body(json);
+            ResponseEntity<String> response = restTemplate.exchange(request, String.class);
+            result = response.getStatusCode().value() < 400;
+        } catch (RuntimeException|URISyntaxException e) {
+            LOGGER.error("POST "+uriPath+" ("+e.getMessage()+")");
+        }
+        return result;
+    }
+
+    @Override
+    public boolean remove(Properties properties) {
+        boolean result = false;
+        String api = properties.getOrDefault("api", "NULL").toString();
+        String json = properties.getOrDefault("json", "{}").toString();
+        String path = properties.getOrDefault("path", "").toString();
+        String uriPath = "http://" + api + "/" + path;
+        CloseableHttpClient httpClient = HttpClientBuilder.create().build();
+        HttpEntityEnclosingRequest delete = new HttpDeleteWithBody("/"+path);
+
+        try {
+            delete.setHeader(HttpHeaders.CONTENT_TYPE, ContentType.APPLICATION_JSON.toString());
+            String[] apiWithPort = api.split(":");
+            String hostName = apiWithPort[0];
+            int port =  apiWithPort.length > 1 ? Integer.valueOf(apiWithPort[1]) : 80;
+            delete.setEntity(new StringEntity(json));
+            HttpResponse response = httpClient.execute(new HttpHost(hostName, port), delete);
+            httpClient.close();
+            result = response.getStatusLine().getStatusCode() < 400;
+        } catch (IOException e) {
+            e.printStackTrace();
+            LOGGER.error("DELETE "+uriPath+" ("+e.getMessage()+")");
+        }
+        return result;
+    }
+
+    @NotThreadSafe
+    private class HttpDeleteWithBody extends HttpEntityEnclosingRequestBase {
+
+        @Override
+        public String getMethod() {
+            return "DELETE";
+        }
+
+        public HttpDeleteWithBody(String uri) {
+            super();
+            setURI(URI.create(uri));
+        }
+    }
 }
