cf2482d56544b3eacff4e5397cc6219b9a97500a
Marcelo Monteiro <m.monteiro@corp.globo.com>
2015-08-10 20:09:11 -0300

    FarmEngine: Provisioning support

--- src/main/java/io/galeb/engine/farm/FarmEngine.java
+++ src/main/java/io/galeb/engine/farm/FarmEngine.java
@@ -7,7 +7,7 @@ import org.apache.commons.logging.LogFactory;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Component;
 
-import io.galeb.engine.Driver;
+import io.galeb.engine.Provisioning;
 import io.galeb.entity.AbstractEntity;
 import io.galeb.entity.Farm;
 
@@ -25,29 +25,17 @@ public class FarmEngine extends AbstractEngine {
         return Optional.empty();
     }
 
-    @Override
-    protected String makeJson(AbstractEntity<?> entity) {
-        return "{}";
-    }
-
     @JmsListener(destination = QUEUE_CREATE)
     public void create(Farm farm) {
         LOGGER.info("Creating "+farm.getClass().getSimpleName()+" "+farm.getName());
-        Driver driver = getDriver(farm);
-        driver.create(fromEntity(farm));
-    }
-
-    @JmsListener(destination = QUEUE_UPDATE)
-    public void update(Farm farm) {
-        LOGGER.info("Updating "+farm.getClass().getSimpleName()+" "+farm.getName());
-        Driver driver = getDriver(farm);
-        driver.update(fromEntity(farm));
+        Provisioning provisioning = getProvisioning(farm);
+        provisioning.create(fromEntity(farm));
     }
 
     @JmsListener(destination = QUEUE_REMOVE)
     public void remove(Farm farm) {
         LOGGER.info("Removing "+farm.getClass().getSimpleName()+" "+farm.getName());
-        Driver driver = getDriver(farm);
-        driver.remove(fromEntity(farm));
+        Provisioning provisioning = getProvisioning(farm);
+        provisioning.create(fromEntity(farm));
     }
 }
