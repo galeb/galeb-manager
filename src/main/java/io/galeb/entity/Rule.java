4a5476c95ab401bc73fa246675f44f379fb645a7
Igor Fernandes <igor.fernandes@corp.globo.com>
2015-08-10 20:29:41 -0300

    Add Farm relationship

--- src/main/java/io/galeb/entity/Rule.java
+++ src/main/java/io/galeb/entity/Rule.java
@@ -1,13 +1,17 @@
 package io.galeb.entity;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
 
 import org.springframework.util.Assert;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 @Entity
-public class Rule extends EntityAffiliable<Rule> {
+public class Rule extends AbstractEntity<Rule> {
 
     private static final long serialVersionUID = 5596582746795373020L;
 
@@ -15,10 +19,18 @@ public class Rule extends EntityAffiliable<Rule> {
     @JoinColumn(nullable = false)
     private RuleType ruleType;
 
-    public Rule(String name, RuleType ruleType, String parent) {
+    @ManyToOne(fetch = FetchType.EAGER)
+    private Environment environment;
+
+    @JsonIgnore
+    private long farmId;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    private VirtualHost parent;
+
+    public Rule(String name, RuleType ruleType, Environment environment) {
         Assert.notNull(ruleType);
         setName(name);
-        setParent(parent);
         this.ruleType = ruleType;
     }
 
@@ -32,6 +44,34 @@ public class Rule extends EntityAffiliable<Rule> {
 
     public Rule setRuleType(RuleType ruleType) {
         this.ruleType = ruleType;
+        return this;
+    }
+
+    public Environment getEnvironment() {
+        return environment;
+    }
+
+    public Rule setEnvironment(Environment environment) {
+        this.environment = environment;
+        return this;
+    }
+
+    public long getFarmId() {
+        return farmId;
+    }
+
+    public Rule setFarmId(long farmId) {
+        this.farmId = farmId;
+        return this;
+    }
+
+    public VirtualHost getParent() {
+        return parent;
+    }
+
+    public Rule setParent(VirtualHost parent) {
+        Assert.notNull(parent);
+        this.parent = parent;
         return this;
     }
 
