309ec7f8aaae5388c7d3edb70febc7d3f819188d
tuxmonteiro <marcelotmonteiro@gmail.com>
2015-08-14 08:57:54 -0300

    SpringSecurityAuditorAware implemented

---
+++ src/main/java/io/galeb/security/SpringSecurityAuditorAware.java
@@ -0,0 +1,27 @@
+package io.galeb.security;
+
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class SpringSecurityAuditorAware implements AuditorAware<String> {
+
+    @Override
+    public String getCurrentAuditor() {
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String currentUser = "UNDEF";
+
+        if (authentication != null && authentication.isAuthenticated()) {
+            Object principal = authentication.getPrincipal();
+            if (principal instanceof UserDetails) {
+                currentUser = ((UserDetails)principal).getUsername();
+            } else {
+                currentUser = principal.toString();
+            }
+        }
+
+        return currentUser;
+    }
+}
