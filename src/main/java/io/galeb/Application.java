89c1360620d067b6da216a0687aeb7f199e5a6d8
Marcelo Monteiro <m.monteiro@corp.globo.com>
2015-08-14 14:56:13 -0300

    Application: removed bodyOn[Create|Update] and show Account.id & Team.id

--- src/main/java/io/galeb/Application.java
+++ src/main/java/io/galeb/Application.java
@@ -10,6 +10,7 @@ import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestMvcConfiguration;
 import org.springframework.jms.annotation.EnableJms;
 
+import io.galeb.entity.Account;
 import io.galeb.entity.Environment;
 import io.galeb.entity.Farm;
 import io.galeb.entity.Project;
@@ -17,6 +18,7 @@ import io.galeb.entity.Rule;
 import io.galeb.entity.RuleType;
 import io.galeb.entity.Target;
 import io.galeb.entity.TargetType;
+import io.galeb.entity.Team;
 import io.galeb.entity.VirtualHost;
 
 @SpringBootApplication
@@ -37,16 +39,16 @@ public class Application {
 
     @PostConstruct
     public void postConstruct() {
-        repositoryRestConfiguration.setReturnBodyOnCreate(true)
-                                   .setReturnBodyOnUpdate(true)
-                                   .exposeIdsFor(Environment.class,
+        repositoryRestConfiguration.exposeIdsFor(Environment.class,
                                                  Farm.class,
                                                  Project.class,
                                                  Rule.class,
                                                  RuleType.class,
                                                  Target.class,
                                                  TargetType.class,
-                                                 VirtualHost.class);
+                                                 VirtualHost.class,
+                                                 Account.class,
+                                                 Team.class);
     }
 
 }
