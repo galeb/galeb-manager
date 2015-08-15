b45eedc3468f4112a3b0565a3c6f5d20e5eab2d9
tuxmonteiro <marcelotmonteiro@gmail.com>
2015-08-15 11:04:45 -0300

    Fix CurrentUser

--- src/main/java/io/galeb/security/CurrentUser.java
+++ src/main/java/io/galeb/security/CurrentUser.java
@@ -1,6 +1,5 @@
 package io.galeb.security;
 
-import java.util.EnumSet;
 import java.util.stream.Collectors;
 
 import org.springframework.security.core.authority.AuthorityUtils;
@@ -12,14 +11,12 @@ public class CurrentUser extends User {
 
     private static final long serialVersionUID = -403060077273343289L;
 
-    private static String[] allRoles = EnumSet.allOf(Account.Role.class).stream()
-            .map(c -> c.toString()).collect(
-                    Collectors.toList()).toArray(new String[EnumSet.allOf(Account.Role.class).size()-1]);
-
     public CurrentUser(Account account) {
         super(account.getName(),
               "password",
-              AuthorityUtils.createAuthorityList(allRoles)
+              AuthorityUtils.createAuthorityList(account.getRoles().stream()
+                      .map(role -> role.toString()).collect(Collectors.toList())
+                      .toArray(new String[account.getRoles().size()-1]))
               );
     }
 
