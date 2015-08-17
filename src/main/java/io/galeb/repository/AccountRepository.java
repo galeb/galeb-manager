c7b9435f84bbc12f3a880aa51ce092e891739ca9
tuxmonteiro <marcelotmonteiro@gmail.com>
2015-08-17 08:48:21 -0300

    Repository: initial query filter support

--- src/main/java/io/galeb/repository/AccountRepository.java
+++ src/main/java/io/galeb/repository/AccountRepository.java
@@ -2,6 +2,7 @@ package io.galeb.repository;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
@@ -9,6 +10,21 @@ import io.galeb.entity.Account;
 
 @RepositoryRestResource(collectionResourceRel = "account", path = "account")
 public interface AccountRepository extends PagingAndSortingRepository<Account, Long> {
+
+    @Query("SELECT a FROM Account a WHERE "
+            + "a.id = :id AND "
+                + "(1 = ?#{hasRole('ADMIN') ? 1 : 0 } OR "
+                + "a.createdBy = ?#{principal.username} OR "
+                + "a.name = ?#{principal.username})")
+    @Override
+    Account findOne(@Param("id") Long id);
+
+    @Query("SELECT a FROM Account a WHERE "
+            + "1 = ?#{hasRole('ADMIN') ? 1 : 0 } OR "
+            + "a.createdBy = ?#{principal.username} OR "
+            + "a.name = ?#{principal.username}")
+    @Override
+    Iterable<Account> findAll();
 
     List<Account> findByName(@Param("name") String name);
 
--- src/main/java/io/galeb/repository/ProjectRepository.java
+++ src/main/java/io/galeb/repository/ProjectRepository.java
@@ -2,14 +2,20 @@ package io.galeb.repository;
 
 import java.util.List;
 
+//import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-
 import io.galeb.entity.Project;
 
 @RepositoryRestResource(collectionResourceRel = "project", path = "project")
 public interface ProjectRepository extends PagingAndSortingRepository<Project, Long> {
+
+//    @Query("SELECT p FROM Project p "
+//            + "INNER JOIN p.teams t "
+//            + "INNER JOIN t.accounts a WHERE a in ?#{principal.username}")
+//    @Override
+//    Project findOne(Long id);
 
     List<Project> findByName(@Param("name") String name);
 
