8dcad6342aba942e957b7fdc6043cbf724af0926
Marcelo Monteiro <m.monteiro@corp.globo.com>
2015-08-17 18:39:57 -0300

    ProjectRepository: initial query filter

--- src/main/java/io/galeb/repository/ProjectRepository.java
+++ src/main/java/io/galeb/repository/ProjectRepository.java
@@ -2,20 +2,29 @@ package io.galeb.repository;
 
 import java.util.List;
 
-//import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
 import io.galeb.entity.Project;
 
 @RepositoryRestResource(collectionResourceRel = "project", path = "project")
 public interface ProjectRepository extends PagingAndSortingRepository<Project, Long> {
 
-//    @Query("SELECT p FROM Project p "
-//            + "INNER JOIN p.teams t "
-//            + "INNER JOIN t.accounts a WHERE a in ?#{principal.username}")
-//    @Override
-//    Project findOne(Long id);
+    @Override
+    @Query("SELECT p FROM Project p "
+            + "INNER JOIN p.teams t "
+            + "INNER JOIN t.accounts a "
+            + "WHERE p.id = :id")
+    Project findOne(@Param("id") Long id);
+
+    @Override
+    @Query("SELECT p FROM Project p "
+            + "INNER JOIN p.teams t "
+            + "INNER JOIN t.accounts a "
+            + "WHERE 1=1")
+    List<Project> findAll();
 
     List<Project> findByName(@Param("name") String name);
 
