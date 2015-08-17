393b72c019aed80d490aa9ad7b98842e36823bb0
tuxmonteiro <marcelotmonteiro@gmail.com>
2015-08-17 08:44:21 -0300

    Repository: spring security query support

---
+++ src/main/java/io/galeb/security/RepositorySecurityConfiguration.java
@@ -0,0 +1,17 @@
+package io.galeb.security;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.data.repository.query.SecurityEvaluationContextExtension;
+
+@Configuration
+@EnableAutoConfiguration
+public class RepositorySecurityConfiguration {
+
+    @Bean
+    public SecurityEvaluationContextExtension securityEvaluationContextExtension(){
+        return new SecurityEvaluationContextExtension();
+    }
+
+}
