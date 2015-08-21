44d6f0dc5bdb7896a0a295acaf367eee5f3a04f3
tuxmonteiro <marcelotmonteiro@gmail.com>
2015-08-21 10:20:36 -0300

    TargetRepository: PreAuthorize applied

--- src/main/java/io/galeb/manager/repository/TargetRepository.java
+++ src/main/java/io/galeb/manager/repository/TargetRepository.java
@@ -23,10 +23,12 @@ import java.util.List;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.security.access.prepost.PreAuthorize;
 
 import io.galeb.manager.entity.Rule;
 import io.galeb.manager.entity.Target;
 
+@PreAuthorize("isFullyAuthenticated()")
 @RepositoryRestResource(collectionResourceRel = "target", path = "target")
 public interface TargetRepository extends PagingAndSortingRepository<Target, Long> {
 
