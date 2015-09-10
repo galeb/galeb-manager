f8d9e972b014c1e981f36d82bd5d8d8c97ca9111
Marcelo Monteiro (tuxmonteiro) <marcelotmonteiro@gmail.com>
2015-09-10 15:02:31 -0300

    Fix AbstractEntity.equals

--- src/main/java/io/galeb/manager/entity/AbstractEntity.java
+++ src/main/java/io/galeb/manager/entity/AbstractEntity.java
@@ -207,8 +207,6 @@ public abstract class AbstractEntity<T extends AbstractEntity<?>> implements Ser
     @SuppressWarnings("unchecked")
     @Override
     public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
         if (obj == null)
             return false;
         if (getClass() != obj.getClass())
