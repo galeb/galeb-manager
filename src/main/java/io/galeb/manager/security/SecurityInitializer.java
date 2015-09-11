8aa189b7c248821c3b61357b65438534f63a76dd
Marcelo Monteiro (tuxmonteiro) <marcelotmonteiro@gmail.com>
2015-09-11 13:38:34 -0300

    Redis support external configuration & Fix SimpleCORSFilter

--- src/main/java/io/galeb/manager/security/SecurityInitializer.java
+++ src/main/java/io/galeb/manager/security/SecurityInitializer.java
@@ -20,7 +20,6 @@ package io.galeb.manager.security;
 
 import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
 
-public class SecurityInitializer extends
-		AbstractSecurityWebApplicationInitializer {
+public class SecurityInitializer extends AbstractSecurityWebApplicationInitializer {
 
 }
