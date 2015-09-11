d82148dbf959653c9d683a38b9fc91873e980876
Marcelo Monteiro (tuxmonteiro) <marcelotmonteiro@gmail.com>
2015-09-11 16:41:03 -0300

    CORS attributes externalized

--- src/main/java/io/galeb/manager/security/SimpleCORSFilter.java
+++ src/main/java/io/galeb/manager/security/SimpleCORSFilter.java
@@ -20,15 +20,36 @@ import org.springframework.stereotype.Component;
 @Order(Ordered.HIGHEST_PRECEDENCE)
 public class SimpleCORSFilter implements Filter {
 
+    private enum SystemEnvCors {
+        CORS_ALLOW_ORIGIN,
+        CORS_ALLOW_METHODS,
+        CORS_ALLOW_HEADERS,
+        CORS_MAX_AGE
+    }
+
     private static final Log LOGGER = LogFactory.getLog(SimpleCORSFilter.class);
+    private static final String DEFAULT_ALLOW_ORIGIN = "*";
+    private static final String DEFAULT_ALLOW_METHODS = "POST, GET, OPTIONS, DELETE, PUT, PATCH";
+    private static final String DEFAULT_MAX_AGE = "3600";
+    private static final String DEFAULT_ALLOW_HEADERS = "x-requested-with, authorization, "
+                                                      + "content-type, x-auth-token";
 
     @Override
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
         final HttpServletResponse response = (HttpServletResponse) res;
-        response.setHeader("Access-Control-Allow-Origin", "*");
-        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT, PATCH");
-        response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers", "x-requested-with, authorization, content-type, x-auth-token");
+        String allowOrigin = System.getenv(SystemEnvCors.CORS_ALLOW_ORIGIN.toString());
+        allowOrigin = allowOrigin == null ? DEFAULT_ALLOW_ORIGIN : allowOrigin;
+        String allowMethods = System.getenv(SystemEnvCors.CORS_ALLOW_METHODS.toString());
+        allowMethods = allowMethods == null ? DEFAULT_ALLOW_METHODS : allowMethods;
+        String allowHeaders = System.getenv(SystemEnvCors.CORS_ALLOW_HEADERS.toString());
+        allowHeaders = allowHeaders == null ? DEFAULT_ALLOW_HEADERS : allowHeaders;
+        String maxAge = System.getenv(SystemEnvCors.CORS_MAX_AGE.toString());
+        maxAge = maxAge == null ? DEFAULT_MAX_AGE : maxAge;
+
+        response.setHeader("Access-Control-Allow-Origin", allowOrigin);
+        response.setHeader("Access-Control-Allow-Methods", allowMethods);
+        response.setHeader("Access-Control-Max-Age", maxAge);
+        response.setHeader("Access-Control-Allow-Headers", allowHeaders);
         try {
             chain.doFilter(req, res);
         } catch (Exception e) {
