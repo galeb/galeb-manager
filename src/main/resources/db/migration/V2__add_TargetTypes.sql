8d684de0fcb00e80b940d912ff284fb60a0fb1f9
Marcelo Monteiro (tuxmonteiro) <marcelotmonteiro@gmail.com>
2015-09-14 23:30:45 -0300

    TargetTypePreLoad removed. Applied best approach with Flyway

---
+++ src/main/resources/db/migration/V2__add_TargetTypes.sql
@@ -0,0 +1,28 @@
+-- MySQL dump 10.13  Distrib 5.6.25, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: galeb
+-- ------------------------------------------------------
+-- Server version	5.6.25-0ubuntu0.15.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Dumping data for table `targettype`
+--
+
+LOCK TABLES `targettype` WRITE;
+/*!40000 ALTER TABLE `targettype` DISABLE KEYS */;
+INSERT INTO `targettype` VALUES (3,'2015-09-14 23:05:55','admin','2015-09-14 23:05:55','admin','Backend',1,0,NULL),(4,'2015-09-14 23:06:02','admin','2015-09-14 23:06:02','admin','BackendPool',1,0,NULL);
+/*!40000 ALTER TABLE `targettype` ENABLE KEYS */;
+UNLOCK TABLES;
+
+-- Dump completed on 2015-09-14 23:06:52
