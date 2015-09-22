3c332984bbec07b97428d108b8adf14c8cc80b40
Marcelo Monteiro <m.monteiro@corp.globo.com>
2015-09-22 15:58:00 -0300

    Improve tokenController

--- src/main/java/io/galeb/manager/controller/TokenController.java
+++ src/main/java/io/galeb/manager/controller/TokenController.java
@@ -2,19 +2,50 @@ package io.galeb.manager.controller;
 
 import javax.servlet.http.HttpSession;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.galeb.manager.security.CurrentUser;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.http.MediaType;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 @RestController
 public class TokenController {
+
+    private static final Log LOGGER = LogFactory.getLog(TokenController.class);
+
+    private final ObjectMapper mapper = new ObjectMapper();
 
     @RequestMapping(value = "/token", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     @PreAuthorize("isFullyAuthenticated()")
     public String token(HttpSession session) {
-        return "{\"token\":\""+session.getId()+"\"}";
+
+        Authentication currentUser = CurrentUser.getCurrentAuth();
+
+        Map<String, Object> tokenInfo = new HashMap<>();
+        tokenInfo.put("token", session.getId());
+        tokenInfo.put("account", currentUser.getName());
+        Set<String> roles = currentUser.getAuthorities().stream().map(GrantedAuthority::getAuthority).collect(Collectors.toSet());
+        tokenInfo.put("admin", roles.contains("ROLE_ADMIN"));
+        String json = "{}";
+        try {
+            json = mapper.writeValueAsString(tokenInfo);
+        } catch (JsonProcessingException e) {
+            LOGGER.error(e);
+        }
+
+        return json;
     }
 
 }
