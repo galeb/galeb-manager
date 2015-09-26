e3ad634fffdee629d743d70fd118219c5abc2f9f
Igor Fernandes <igor.fernandes@corp.globo.com>
2015-09-26 14:07:06 -0300

    Fix repositories import

--- src/main/java/io/galeb/manager/repository/BalancePolicyRepository.java
+++ src/main/java/io/galeb/manager/repository/BalancePolicyRepository.java
@@ -21,6 +21,7 @@ package io.galeb.manager.repository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.security.access.prepost.PreAuthorize;
 
