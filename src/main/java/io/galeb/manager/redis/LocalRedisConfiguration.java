5e2c8a88ae2af1c27c5b6f154377057769eb021f
Marcelo Monteiro (tuxmonteiro) <marcelotmonteiro@gmail.com>
2015-09-30 09:09:21 -0300

    Redis Sentinel support

--- src/main/java/io/galeb/manager/redis/LocalRedisConfiguration.java
+++ src/main/java/io/galeb/manager/redis/LocalRedisConfiguration.java
@@ -6,7 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
+import org.springframework.data.redis.connection.*;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+
+import java.util.*;
+import java.util.stream.*;
 
 @Configuration
 public class LocalRedisConfiguration {
@@ -18,7 +22,6 @@ public class LocalRedisConfiguration {
 
     @Bean
     public JedisConnectionFactory connectionFactory() {
-        JedisConnectionFactory jedisConnectionFactory = new JedisConnectionFactory();
         String hostName = System.getenv("REDIS_HOSTNAME");
         hostName = hostName == null ?
                 env.getProperty("spring.redis.host", "127.0.0.1") : hostName;
@@ -34,6 +37,37 @@ public class LocalRedisConfiguration {
         String connectionTimeout = System.getenv("REDIS_TIMEOUT");
         connectionTimeout = connectionTimeout == null ?
                 env.getProperty("spring.redis.timeout", "") : connectionTimeout;
+
+        JedisConnectionFactory jedisConnectionFactory = null;
+
+        String useSentinel = System.getenv("REDIS_USE_SENTINEL");
+        useSentinel = useSentinel == null ? "false" : useSentinel;
+
+        RedisSentinelConfiguration sentinelConfig = null;
+        if (Boolean.getBoolean(useSentinel)) {
+            String masterName = System.getenv("REDIS_SENTINEL_MASTER_NAME");
+            masterName = masterName == null ?
+                    env.getProperty("spring.redis.sentinel.master", "mymaster") : masterName;
+            String redisSentinelNodes = System.getenv("REDIS_SENTINEL_NODES");
+            redisSentinelNodes = redisSentinelNodes == null ?
+                    env.getProperty("spring.redis.sentinel.nodes", "127.0.0.1:26379") : redisSentinelNodes;
+            List<String> redisSentinelNodesStringList = Arrays.asList(redisSentinelNodes.split(","));
+            try {
+                Iterable<RedisNode> redisSentinelNodesList = redisSentinelNodesStringList.stream().map(node ->
+                                new RedisNode(node.split(":")[0], Integer.parseInt(node.split(":")[1]))
+                ).collect(Collectors.toList());
+                sentinelConfig = new RedisSentinelConfiguration();
+                sentinelConfig.master(masterName).setSentinels(redisSentinelNodesList);
+            } catch (Exception e) {
+                LOGGER.error(e);
+            }
+        }
+
+        if (sentinelConfig != null) {
+            jedisConnectionFactory = new JedisConnectionFactory(sentinelConfig);
+        } else {
+            jedisConnectionFactory = new JedisConnectionFactory();
+        }
 
         jedisConnectionFactory.setHostName(hostName);
         try {
