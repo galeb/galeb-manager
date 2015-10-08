2f62575717b32ba569e6a66b7e352f675137f9ff
Marcelo Monteiro (tuxmonteiro) <marcelotmonteiro@gmail.com>
2015-10-08 09:33:22 -0300

    Fix Interface missing

---
+++ src/main/java/io/galeb/manager/repository/JpaRepositoryWithFindByName.java
@@ -0,0 +1,34 @@
+/*
+ * Galeb - Load Balance as a Service Plataform
+ *
+ * Copyright (C) 2014-2015 Globo.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ *
+ */
+
+package io.galeb.manager.repository;
+
+import org.springframework.data.domain.*;
+import org.springframework.data.jpa.repository.*;
+import org.springframework.data.repository.*;
+
+import java.io.Serializable;
+
+@NoRepositoryBean
+public interface JpaRepositoryWithFindByName<T, ID extends Serializable> extends JpaRepository<T, ID> {
+
+    Page<T> findByName(String name, Pageable pageable);
+
+}
