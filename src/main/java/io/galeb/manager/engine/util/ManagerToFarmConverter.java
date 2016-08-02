b2ac6ff60c73a7699df9d220384a5fa0da34bf76
Tiago Machado <tiago.machado@corp.globo.com>
2016-08-02 17:42:41 -0300

    Fix the package of entities

--- src/main/java/io/galeb/manager/engine/util/ManagerToFarmConverter.java
+++ src/main/java/io/galeb/manager/engine/util/ManagerToFarmConverter.java
@@ -1,13 +1,6 @@
 package io.galeb.manager.engine.util;
 
-import io.galeb.core.model.Backend;
-import io.galeb.core.model.BackendPool;
 import io.galeb.core.model.Entity;
-import io.galeb.core.model.Farm;
-import io.galeb.core.model.Rule;
-import io.galeb.core.model.VirtualHost;
-import io.galeb.manager.entity.Pool;
-import io.galeb.manager.entity.Target;
 
 import java.util.Arrays;
 import java.util.HashMap;
@@ -18,7 +11,10 @@ import java.util.stream.Collectors;
 public class ManagerToFarmConverter {
 
     public static final List<Class<? extends Entity>> FARM_ENTITIES_LIST =
-                Arrays.asList(VirtualHost.class, Rule.class, BackendPool.class, Backend.class);
+                Arrays.asList(io.galeb.core.model.VirtualHost.class,
+                        io.galeb.core.model.Rule.class,
+                        io.galeb.core.model.BackendPool.class,
+                        io.galeb.core.model.Backend.class);
 
     public static final Map<String, Class<? extends Entity>> FARM_ENTITY_MAP = FARM_ENTITIES_LIST.stream()
             .collect(Collectors.toMap(clazz -> clazz.getSimpleName().toLowerCase(), clazz -> clazz));
@@ -28,17 +24,17 @@ public class ManagerToFarmConverter {
     public static final Map<String, Class<?>> FARM_TO_MANAGER_ENTITY_MAP = new HashMap<>();
 
     static {
-        MANAGER_TO_FARM_ENTITY_MAP.put(VirtualHost.class.getSimpleName().toLowerCase(), VirtualHost.class);
-        MANAGER_TO_FARM_ENTITY_MAP.put(Pool.class.getSimpleName().toLowerCase(), BackendPool.class);
-        MANAGER_TO_FARM_ENTITY_MAP.put(Rule.class.getSimpleName().toLowerCase(), Rule.class);
-        MANAGER_TO_FARM_ENTITY_MAP.put(Target.class.getSimpleName().toLowerCase(), Backend.class);
-        MANAGER_TO_FARM_ENTITY_MAP.put(Farm.class.getSimpleName().toLowerCase(), Farm.class);
+        MANAGER_TO_FARM_ENTITY_MAP.put(io.galeb.manager.entity.VirtualHost.class.getSimpleName().toLowerCase(), io.galeb.core.model.VirtualHost.class);
+        MANAGER_TO_FARM_ENTITY_MAP.put(io.galeb.manager.entity.Pool.class.getSimpleName().toLowerCase(), io.galeb.core.model.BackendPool.class);
+        MANAGER_TO_FARM_ENTITY_MAP.put(io.galeb.manager.entity.Rule.class.getSimpleName().toLowerCase(), io.galeb.core.model.Rule.class);
+        MANAGER_TO_FARM_ENTITY_MAP.put(io.galeb.manager.entity.Target.class.getSimpleName().toLowerCase(), io.galeb.core.model.Backend.class);
+        MANAGER_TO_FARM_ENTITY_MAP.put(io.galeb.manager.entity.Farm.class.getSimpleName().toLowerCase(), io.galeb.core.model.Farm.class);
 
-        FARM_TO_MANAGER_ENTITY_MAP.put(VirtualHost.class.getSimpleName().toLowerCase(), VirtualHost.class);
-        FARM_TO_MANAGER_ENTITY_MAP.put(BackendPool.class.getSimpleName().toLowerCase(), Pool.class);
-        FARM_TO_MANAGER_ENTITY_MAP.put(Rule.class.getSimpleName().toLowerCase(), Rule.class);
-        FARM_TO_MANAGER_ENTITY_MAP.put(Backend.class.getSimpleName().toLowerCase(), Target.class);
-        FARM_TO_MANAGER_ENTITY_MAP.put(Farm.class.getSimpleName().toLowerCase(), Farm.class);
+        FARM_TO_MANAGER_ENTITY_MAP.put(io.galeb.core.model.VirtualHost.class.getSimpleName().toLowerCase(), io.galeb.manager.entity.VirtualHost.class);
+        FARM_TO_MANAGER_ENTITY_MAP.put(io.galeb.core.model.BackendPool.class.getSimpleName().toLowerCase(), io.galeb.manager.entity.Pool.class);
+        FARM_TO_MANAGER_ENTITY_MAP.put(io.galeb.core.model.Rule.class.getSimpleName().toLowerCase(), io.galeb.manager.entity.Rule.class);
+        FARM_TO_MANAGER_ENTITY_MAP.put(io.galeb.core.model.Backend.class.getSimpleName().toLowerCase(), io.galeb.manager.entity.Target.class);
+        FARM_TO_MANAGER_ENTITY_MAP.put(io.galeb.core.model.Farm.class.getSimpleName().toLowerCase(), io.galeb.manager.entity.Farm.class);
     }
 
     private ManagerToFarmConverter() {
