24ea51762a2699b19f737762a1f9f794ee63382e
Marcelo Monteiro (tuxmonteiro) <marcelotmonteiro@gmail.com>
2016-09-11 21:41:50 -0300

    Added initial GalebV32Driver Pool tests

--- src/test/java/io/galeb/manager/engine/driver/impl/GalebV32DriverTest.java
+++ src/test/java/io/galeb/manager/engine/driver/impl/GalebV32DriverTest.java
@@ -19,12 +19,14 @@
 package io.galeb.manager.engine.driver.impl;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import io.galeb.core.model.BackendPool;
 import io.galeb.manager.common.JsonMapper;
 import io.galeb.manager.common.Properties;
 import io.galeb.manager.engine.driver.Driver;
 import io.galeb.manager.engine.driver.DriverBuilder;
 import io.galeb.manager.entity.Environment;
 import io.galeb.manager.entity.Farm;
+import io.galeb.manager.entity.Pool;
 import io.galeb.manager.entity.Provider;
 import io.galeb.manager.httpclient.FakeHttpClient;
 import org.apache.commons.lang3.exception.ExceptionUtils;
@@ -82,6 +84,54 @@ public class GalebV32DriverTest {
         Assert.isTrue(!resultUpdate, "Update Farm is possible?");
     }
 
+    @Test
+    public void poolNotExist() {
+        logTestedMethod();
+        boolean result = driver.exist(poolPropertiesBuild(newPool()));
+        Assert.isTrue(!result);
+    }
+
+    @Test
+    public void createPool() {
+        logTestedMethod();
+        Properties properties = poolPropertiesBuild(newPool());
+        boolean resultCreate = driver.create(properties);
+        boolean resultExist = driver.exist(properties);
+        Assert.isTrue(resultCreate && resultExist);
+    }
+
+    @Test
+    public void updatePool() throws Exception {
+        logTestedMethod();
+        Pool pool = newPool();
+        Properties properties = poolPropertiesBuild(pool);
+        String api = properties.getOrDefault("api", "UNDEF").toString();
+        String poolName = pool.getName();
+        boolean resultCreate = driver.create(properties);
+        Map<String, String> map = driver.getAll(properties).get("backendpool").get(api + "/backendpool/" + poolName + "@");
+        String versionStr = map.get("version");
+        int versionOrig = Integer.parseInt(versionStr);
+        boolean resultExist = driver.exist(properties);
+        pool.updateHash();
+        boolean resultUpdate = driver.update(poolPropertiesBuild(pool));
+        map = driver.getAll(properties).get("backendpool").get(api + "/backendpool/" + poolName + "@");
+        versionStr = map.get("version");
+        int versionNew = Integer.parseInt(versionStr);
+        Assert.isTrue(resultCreate && resultExist && resultUpdate && versionNew > versionOrig);
+    }
+
+    @Test
+    public void removePool() {
+        logTestedMethod();
+        Properties properties = poolPropertiesBuild(newPool());
+        boolean resultCreate = driver.create(properties);
+        boolean resultExist = driver.exist(properties);
+        boolean resultRemove = driver.remove(properties);
+        boolean resultNotExist = !driver.exist(properties);
+
+        Assert.isTrue(resultCreate && resultExist && resultRemove && resultNotExist);
+    }
+
     private Farm newFarm(String api) {
         Environment environment = new Environment("NULL");
         Provider provider = new Provider(GalebV32Driver.class.getSimpleName());
@@ -106,6 +156,30 @@ public class GalebV32DriverTest {
 
         return properties;
     }
+
+    private Pool newPool() {
+        final Pool pool= new Pool(UUID.randomUUID().toString());
+        pool.getProperties().put(BackendPool.PROP_LOADBALANCE_POLICY, "DEFAULT");
+        pool.updateHash();
+        return pool;
+    }
+
+    private Properties poolPropertiesBuild(Pool pool) {
+        String api = "api";
+        Map<String, List<?>> entitiesMap = Collections.emptyMap();
+        Properties properties = new Properties();
+        properties.put("api", api);
+        properties.put("entitiesMap", entitiesMap);
+        properties.put("path", BackendPool.class.getSimpleName().toLowerCase());
+        try {
+            properties.put("json", new JsonMapper().makeJson(pool).toString());
+        } catch (JsonProcessingException e) {
+            LOGGER.error(ExceptionUtils.getStackTrace(e));
+        }
+
+        return properties;
+    }
+
 
     private void logTestedMethod() {
         LOGGER.info("Testing " + this.getClass().getSimpleName() + "." + Thread.currentThread().getStackTrace()[2].getMethodName());
