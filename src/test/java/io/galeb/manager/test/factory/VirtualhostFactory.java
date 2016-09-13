b67ee9762fb3e0cbb890059b80d6c6d492b97a77
Marcelo Monteiro (tuxmonteiro) <marcelotmonteiro@gmail.com>
2016-09-13 16:15:12 -0300

    Added VirtualhostFactory

---
+++ src/test/java/io/galeb/manager/test/factory/VirtualhostFactory.java
@@ -0,0 +1,51 @@
+/*
+ * Galeb - Load Balance as a Service Plataform
+ *
+ * Copyright (C) 2014-2016 Globo.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.galeb.manager.test.factory;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import io.galeb.manager.common.JsonMapper;
+import io.galeb.manager.common.Properties;
+import io.galeb.manager.engine.listeners.VirtualHostEngine;
+import io.galeb.manager.entity.Environment;
+import io.galeb.manager.entity.Pool;
+import io.galeb.manager.entity.Project;
+import io.galeb.manager.entity.VirtualHost;
+import org.apache.commons.lang3.exception.ExceptionUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import java.util.*;
+
+public class VirtualhostFactory {
+
+    private static final Log LOGGER = LogFactory.getLog(VirtualhostFactory.class);
+
+    public VirtualHost build(String name) {
+        Environment environment = new Environment("NULL");
+        Project project = new Project("NULL");
+        return new VirtualHost(name, environment, project);
+    }
+
+    public Properties makeProperties(VirtualHost virtualHost) {
+        Map<String, String> jmsHeaderProperties = new HashMap<>();
+        jmsHeaderProperties.put("api", "api");
+        return new VirtualHostEngine().makeProperties(virtualHost, jmsHeaderProperties);
+    }
+
+}
