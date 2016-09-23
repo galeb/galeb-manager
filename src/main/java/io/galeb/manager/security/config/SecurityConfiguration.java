1235f7d09b1eb554a78b91aaa9649e8b53056f8b
Marcelo Monteiro (tuxmonteiro) <marcelotmonteiro@gmail.com>
2016-09-23 18:33:38 -0300

    Improve auth token (token creation only in /token path)

--- src/main/java/io/galeb/manager/security/config/SecurityConfiguration.java
+++ src/main/java/io/galeb/manager/security/config/SecurityConfiguration.java
@@ -48,8 +48,10 @@ import org.springframework.ldap.core.DirContextOperations;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.AuthorityUtils;
@@ -173,6 +175,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.NEVER);
         http.authorizeRequests().anyRequest().fullyAuthenticated()
             .and()
             .logout().deleteCookies("JSESSIONID","SPRING_SECURITY_REMEMBER_ME_COOKIE")
