2a2374cb985b73097850d3154f0ccfbe5bd0cece
Marcelo Monteiro (tuxmonteiro) <marcelotmonteiro@gmail.com>
2016-09-26 19:30:37 -0300

    Session timeout parameter implemented

--- src/main/java/io/galeb/manager/security/config/SessionRepositoryConfig.java
+++ src/main/java/io/galeb/manager/security/config/SessionRepositoryConfig.java
@@ -1,52 +1,55 @@
+/*
+ * Galeb - Load Balance as a Service Plataform
+ *
+ * Copyright (C) 2014-2016 Globo.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ *
+ */
+
 package io.galeb.manager.security.config;
 
-import org.apache.commons.lang3.exception.ExceptionUtils;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.session.ExpiringSession;
+
 import org.springframework.session.data.redis.RedisOperationsSessionRepository;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 import org.springframework.session.web.http.HeaderHttpSessionStrategy;
 import org.springframework.session.web.http.HttpSessionStrategy;
 
+import javax.annotation.PostConstruct;
+
 @Configuration
 @EnableRedisHttpSession
 public class SessionRepositoryConfig {
 
-    private static final Log LOGGER = LogFactory.getLog(SessionRepositoryConfig.class);
-
-    private static final String SERVER_SESSION_TIMEOUT_PROP = "server.session-timeout";
-    private static final String SERVER_SESSION_TIMEOUT_DEF  = "1800";
-
     @Value("#{systemProperties['server.session-timeout']?:1800}")
     private int maxInactiveIntervalInSeconds;
+
+    @Autowired
+    private RedisOperationsSessionRepository sessionRepository;
 
     @Bean
     public HttpSessionStrategy httpSessionStrategy() {
         return new HeaderHttpSessionStrategy();
     }
 
-    @Primary
-    @Bean
-    RedisOperationsSessionRepository sessionRepository(RedisTemplate<String, ExpiringSession> sessionRedisTemplate) {
-        final RedisOperationsSessionRepository sessionRepository = new RedisOperationsSessionRepository(sessionRedisTemplate);
-        String propertyServerSession = System.getProperty(SERVER_SESSION_TIMEOUT_PROP, SERVER_SESSION_TIMEOUT_DEF);
-        try {
-            if (!propertyServerSession.equals(SERVER_SESSION_TIMEOUT_DEF)) {
-                if (maxInactiveIntervalInSeconds == Integer.parseInt(SERVER_SESSION_TIMEOUT_DEF)) {
-                    maxInactiveIntervalInSeconds = Integer.parseInt(propertyServerSession);
-                }
-                sessionRepository.setDefaultMaxInactiveInterval(maxInactiveIntervalInSeconds);
-            }
-        } catch (Exception e) {
-            LOGGER.error(ExceptionUtils.getStackTrace(e));
-        }
-        return sessionRepository;
+    @PostConstruct
+    private void afterPropertiesSet() {
+        sessionRepository.setDefaultMaxInactiveInterval(maxInactiveIntervalInSeconds);
     }
 
 }
