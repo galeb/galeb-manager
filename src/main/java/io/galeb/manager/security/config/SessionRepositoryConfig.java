cca41ba04a22a7ba2c34de72ee569c47b6ed1162
Marcelo Monteiro (tuxmonteiro) <marcelotmonteiro@gmail.com>
2016-09-26 17:04:59 -0300

    Session timeout parameter initial support

--- src/main/java/io/galeb/manager/security/config/SessionRepositoryConfig.java
+++ src/main/java/io/galeb/manager/security/config/SessionRepositoryConfig.java
@@ -1,7 +1,15 @@
 package io.galeb.manager.security.config;
 
+import org.apache.commons.lang3.exception.ExceptionUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.session.ExpiringSession;
+import org.springframework.session.data.redis.RedisOperationsSessionRepository;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 import org.springframework.session.web.http.HeaderHttpSessionStrategy;
 import org.springframework.session.web.http.HttpSessionStrategy;
@@ -10,9 +18,35 @@ import org.springframework.session.web.http.HttpSessionStrategy;
 @EnableRedisHttpSession
 public class SessionRepositoryConfig {
 
+    private static final Log LOGGER = LogFactory.getLog(SessionRepositoryConfig.class);
+
+    private static final String SERVER_SESSION_TIMEOUT_PROP = "server.session-timeout";
+    private static final String SERVER_SESSION_TIMEOUT_DEF  = "1800";
+
+    @Value("${server.session-timeout}")
+    private int maxInactiveIntervalInSeconds;
+
     @Bean
     public HttpSessionStrategy httpSessionStrategy() {
         return new HeaderHttpSessionStrategy();
+    }
+
+    @Primary
+    @Bean
+    RedisOperationsSessionRepository sessionRepository(RedisTemplate<String, ExpiringSession> sessionRedisTemplate) {
+        final RedisOperationsSessionRepository sessionRepository = new RedisOperationsSessionRepository(sessionRedisTemplate);
+        String propertyServerSession = System.getProperty(SERVER_SESSION_TIMEOUT_PROP, SERVER_SESSION_TIMEOUT_DEF);
+        try {
+            if (!propertyServerSession.equals(SERVER_SESSION_TIMEOUT_DEF)) {
+                if (maxInactiveIntervalInSeconds == Integer.parseInt(SERVER_SESSION_TIMEOUT_DEF)) {
+                    maxInactiveIntervalInSeconds = Integer.parseInt(propertyServerSession);
+                }
+                sessionRepository.setDefaultMaxInactiveInterval(maxInactiveIntervalInSeconds);
+            }
+        } catch (Exception e) {
+            LOGGER.error(ExceptionUtils.getStackTrace(e));
+        }
+        return sessionRepository;
     }
 
 }
