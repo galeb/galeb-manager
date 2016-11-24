7b06135350cbe05b383251fcd4dd2097cf548e1d
Tiago Machado <tiago.machado@corp.globo.com>
2016-11-24 18:16:04 -0200

    Removing annotation unnecessary

--- src/main/java/io/galeb/manager/exceptions/BadRequestException.java
+++ src/main/java/io/galeb/manager/exceptions/BadRequestException.java
@@ -19,9 +19,7 @@
 package io.galeb.manager.exceptions;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value=HttpStatus.BAD_REQUEST)
 public class BadRequestException extends ResourceException {
 
     private static final long serialVersionUID = 6986223937559244217L;
