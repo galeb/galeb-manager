27fab51de8a9a59a71f685dee169b962a43cfd84
Marcelo Monteiro (tuxmonteiro) <marcelotmonteiro@gmail.com>
2017-05-22 20:18:25 -0300

    RouterMap: added exception logging

--- src/main/java/io/galeb/manager/routermap/RouterMapConfiguration.java
+++ src/main/java/io/galeb/manager/routermap/RouterMapConfiguration.java
@@ -16,30 +16,20 @@
 
 package io.galeb.manager.routermap;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import io.galeb.manager.common.ErrorLogger;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.core.StringRedisTemplate;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 import java.util.concurrent.TimeUnit;
 
 @Configuration
 public class RouterMapConfiguration {
 
-    private final StringRedisTemplate redisTemplate;
-
-    @Autowired
-    public RouterMapConfiguration(StringRedisTemplate redisTemplate) {
-        this.redisTemplate = redisTemplate;
-    }
-
     @Bean
-    public RouterMap routerMap() {
-        return new RouterMap();
+    public RouterMap routerMap(StringRedisTemplate redisTemplate) {
+        return new RouterMap(redisTemplate);
     }
 
     public class RouterMap {
@@ -47,22 +37,38 @@ public class RouterMapConfiguration {
         private static final String ROUTER_PREFIX = "routers:";
         private static final int    REGISTER_TTL  = 30000; // ms
 
-        public synchronized int put(String groupId, String localIp) {
-            String key = ROUTER_PREFIX + groupId + ":" + localIp;
-            redisTemplate.opsForValue().set(key, groupId, REGISTER_TTL, TimeUnit.MILLISECONDS);
-            return redisTemplate.keys(ROUTER_PREFIX + groupId + "*").size();
+        private final StringRedisTemplate redisTemplate;
+
+        public RouterMap(final StringRedisTemplate redisTemplate) {
+            this.redisTemplate = redisTemplate;
         }
 
-        public Map<String, Set<String>> get() {
-            final Map<String, Set<String>> routers = new HashMap<>();
-            redisTemplate.keys(ROUTER_PREFIX + "*").forEach(key -> {
-                String groupIdWithLocalIp = key.replaceFirst(ROUTER_PREFIX, "");
-                int groupIdIndex = groupIdWithLocalIp.indexOf(":");
-                String groupId = groupIdWithLocalIp.substring(0, groupIdIndex - 1);
-                String localIp = groupIdWithLocalIp.substring(groupIdIndex + 1, groupIdWithLocalIp.length());
-                routers.computeIfAbsent(groupId, s -> new HashSet<>()).add(localIp);
-            });
-            return routers;
+        public synchronized int put(String groupId, String localIp) {
+            String key = ROUTER_PREFIX + groupId + ":" + localIp;
+            try {
+                redisTemplate.opsForValue().set(key, groupId, REGISTER_TTL, TimeUnit.MILLISECONDS);
+                return redisTemplate.keys(ROUTER_PREFIX + groupId + "*").size();
+            } catch (Exception e) {
+                ErrorLogger.logError(e, this.getClass());
+            }
+            return 1;
+        }
+
+        public synchronized Map<String, Set<String>> get() {
+            try {
+                final Map<String, Set<String>> routers = new HashMap<>();
+                redisTemplate.keys(ROUTER_PREFIX + "*").forEach(key -> {
+                    String groupIdWithLocalIp = key.replaceFirst(ROUTER_PREFIX, "");
+                    int groupIdIndex = groupIdWithLocalIp.indexOf(":");
+                    String groupId = groupIdWithLocalIp.substring(0, groupIdIndex);
+                    String localIp = groupIdWithLocalIp.substring(groupIdIndex + 1, groupIdWithLocalIp.length());
+                    routers.computeIfAbsent(groupId, s -> new HashSet<>()).add(localIp);
+                });
+                return routers;
+            } catch (Exception e) {
+                ErrorLogger.logError(e, this.getClass());
+            }
+            return Collections.emptyMap();
         }
     }
 }
