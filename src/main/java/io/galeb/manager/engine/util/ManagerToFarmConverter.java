be0a4388a7096eab619b7c1162c0230220b8cbdc
Marcelo Monteiro (tuxmonteiro) <marcelotmonteiro@gmail.com>
2017-06-30 05:37:26 -0300

    remove unnecessary constants and maps

--- src/main/java/io/galeb/manager/engine/util/ManagerToFarmConverter.java
+++ src/main/java/io/galeb/manager/engine/util/ManagerToFarmConverter.java
@@ -2,23 +2,11 @@ package io.galeb.manager.engine.util;
 
 import io.galeb.core.model.Entity;
 
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 public class ManagerToFarmConverter {
-
-    public static final List<Class<? extends Entity>> FARM_ENTITIES_LIST = Collections.unmodifiableList(Arrays.asList(
-                        io.galeb.core.model.VirtualHost.class,
-                        io.galeb.core.model.Rule.class,
-                        io.galeb.core.model.BackendPool.class,
-                        io.galeb.core.model.Backend.class));
-
-    public static final Map<String, Class<? extends Entity>> FARM_ENTITY_MAP = Collections.unmodifiableMap(
-            FARM_ENTITIES_LIST.stream().collect(Collectors.toMap(clazz -> clazz.getSimpleName().toLowerCase(), clazz -> clazz)));
 
     public static final Map<String, Class<? extends Entity>> MANAGER_TO_FARM_ENTITY_MAP;
     public static final Map<String, Class<?>> FARM_TO_MANAGER_ENTITY_MAP;
